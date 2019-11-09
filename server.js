e28caa0475dd20355d35c4fece382bdd2b09971f
Joshua Einsel <Joshua.einsel@live.com>
2019-11-09 15:21:58 -0700

    Initiated HTML files

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const PORT = 3000;
+
+app.use(express.json())
+
+questionaire = []
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'));
+
+app.get("/api/submit", function (request, response) {
+    response.json(submit);
+    console.log("Server listening");
+});
+
+app.post("/api/submit", function (request, response) {
+    const newSubmission = request.body;
+
+    console.log(`new submission!: ${newSubmission}`);
+
+    questionaire.push(newSubmission);
+
+    response.json(submit);
+    console.log(submit);
+});
+
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/questionaire", function (request, response) {
+    response.sendFile(path.join(__dirname, "/public/questionaire.html"));
+});
+
+app.listen(PORT, function () {
+    console.log(`App is listening on http://localhost:${PORT}`);
+})
