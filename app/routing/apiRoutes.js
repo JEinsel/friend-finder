c992a5dbf1c76794694715271562712216002976
Joshua Einsel <Joshua.einsel@live.com>
2019-11-12 07:46:56 -0700

    Implementing response and append from apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,7 @@
 const friends = require("../data/friends")
 var path = require("path")
 var sum = 0;
-var converted;
 var totalDifference;
-var answersArr = [];
-var numbersArr = [];
 var userSum = 0;
 
 for (var i = 0; i < friends.length; i++) {
@@ -12,18 +9,14 @@ for (var i = 0; i < friends.length; i++) {
        for (var j = 0; j < friends[i].scores.length; j++) {
            var element = friends[i].scores[j];
            sum += element
-           //save the sum for each element in friends. Maybe as a property in friend object
-        //    console.log("This is the sum for " + friends[i] + ": " + sum)
        }
    }
 
 
 module.exports = function (app) {
-
     app.get("/api/friends", function (request, response) {
         response.json(friends)
     })
-
 
     app.get("/", function (request, response) {
         response.sendFile(path.join(__dirname, "../public/index.html"));
@@ -33,23 +26,15 @@ module.exports = function (app) {
         response.sendFile(path.join(__dirname, "../public/questionaire.html"));
     });
 
-
     app.post("/api/friends", function (request, response) {
-        console.log("Hello World")
-        console.log(request.body)
-        response.send(`Information from user ${request.body} name : ${request.body.userName} photo: ${request.body.photo} score ${request.body.userScore}`)
-
         var userInfo = request.body
         var minimumDifference = 40;
-
-        
         for (let i = 0; i < userInfo.userScore.length; i++) {
             const element = userInfo.userScore[i];
             var numbers = parseInt(element)
             userSum += numbers;
             console.log(`Line 55: ${userSum}`)
         }
-   
         for (var i = 0; i < friends.length; i++) {
             var totalDifference = 0;
             var element = friends[i].scores.length
@@ -64,10 +49,8 @@ module.exports = function (app) {
                 minimumDifference = totalDifference;
                 console.log(`Line 67 ${friendIndex}`);
                 console.log(`Line 68 ${minimumDifference}`);
-
             }
         }
-
-
+        response.send(`friends ${friends} friendIndex ${friendIndex} totalDifference ${totalDifference} `)
     })
 }
